80b38ae6c4fa196314aed5868dc8961d45a4c932
anh.le <anhle1476@gmail.com>
2022-08-03 10:04:14 +0700

    remove target folder

--- target/classes/apps/static.js
+++
@@ -1,7 +0,0 @@
-import {BimServerViewer} from "../viewer/bimserverviewer.js"
-
-export function displayStaticBuffer(domNode, bufferPath) {
-    var canvas = document.getElementById(domNode);
-    let v = new BimServerViewer({quantizeVertices:false}, canvas, window.innerWidth, window.innerHeight, null);
-    v.loadAnnotationsFromPreparedBufferUrl(bufferPath);
-}
